4e246ac44b510fcd04dab97d082405ac40a304c8
TranQuangHa307 <jsmitterha99@gmail.com>
2021-12-12 23:14:38 +0700

    update

--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -6,7 +6,7 @@ export default () => {
             <div className="blog-post">
                 <h2 className="blog-post-title">Đủ điều kiện xét nghiệm khẳng định các trường hợp HIV dương tính</h2>
                 <p className="blog-post-meta">03/12/2021 15:24 bởi&nbsp;
-                    <a href="#">Cục Phòng chống HIV/AIDS</a></p>
+                    <a href="/#">Cục Phòng chống HIV/AIDS</a></p>
                 <p>Căn cứ Nghị định số 75/2016/NĐ-CP ngày 01 tháng 07 năm 2016 của Chính phủ quy định điều kiện thực hiện xét nghiệm HIV;</p>
                 <blockquote>
                     <p>Căn cứ Nghị định số 155/2018/NĐ-CP ngày 12 tháng 11 năm 2018 của Chính phủ về sửa đổi, bổ sung một số quy định liên quan đến điều kiện đầu tư kinh doanh thuộc phạm vi quản lý nhà nước của BộY tế;</p>
@@ -14,7 +14,7 @@ export default () => {
                 <p>Căn cứ Thông tư số 04/2019/TT-BYT ngày 28 tháng 3 năm 2019 của Bộ Y tế về việc quy định việc phân cấp thẩm quyền cấp, điều chỉnh, thu hồi giấy chứng nhận đủ điều kiện xét nghiệm các trường hợp HIV dương tính và đình chỉ hoạt động xét nghiệm khẳng định các trường hợp HIV dương tính;</p>
                 <p>Cục Phòng, chống HIV/AIDS-Bộ Y tế chứng nhận:</p>
                 <p className="blog-post-meta">
-                    <a href="#">
+                    <a href="/#">
                         Xem chi tiết
                     </a>
                 </p>
@@ -23,7 +23,7 @@ export default () => {
             <div className="blog-post">
                 <h2 className="blog-post-title">Xử phạt vi phạm hành chính trong lĩnh vực y tế</h2>
                 <p className="blog-post-meta">03/12/2021 15:24 bởi&nbsp;
-                    <a href="#">Thanh tra Bộ</a></p>
+                    <a href="/#">Thanh tra Bộ</a></p>
                 <p>Căn cứ Điều 57, Điều 68 Luật Xử lý vi phạm hành chính;</p>
                 <blockquote>
                     <p>Căn cứ Nghị định số 176/2013/NĐ-CP ngày 14/11/2013 của Chính phủ về việc xử phạt vi phạm hành chính trong lĩnh vực y tế;</p>
@@ -31,7 +31,7 @@ export default () => {
                 <p>Căn cứ Biên bản vi phạm hành chính do Thanh tra Bộ Y tế lập vào hồi 09 giờ 30 phút ngày 30/11/2021 tại phòng họp Thanh tra Bộ Y tế, địa chỉ số 138B Giảng Võ, Ba Đình, Hà Nội;</p>
                 <p>Căn cứ Quyết định về việc giao quyền xử phạt vi phạm hành chính số 13/QĐ-TTrB ngày 17/01/2020 của Chánh Thanh tra Bộ Y tế;</p>
                 <p className="blog-post-meta">
-                    <a href="#">
+                    <a href="/#">
                         Xem chi tiết
                     </a>
                 </p>
@@ -48,7 +48,7 @@ export default () => {
                 <p>Phối hợp với các cơ quan đơn vị chức năng trên địa bàn kiểm tra, giám sát việc thu gom chất thải y tế trong phòng chống dịch Covid-19 đặc biệt là vỏ lọ vắc xin phòng Covid-19 theo đúng quy định.</p>
                 <p>Tiếp tục triển khai các hoạt động kiểm tra, thanh tra về giá test  kít  xét nghiệm và giá dịch vụ xét nghiệm, việc kinh doanh và sử dụng các thuốc, vật tư, trang thiết bị phòng chống dịch đảm bảo theo đúng các quy định hiện hành.</p>
                 <p className="blog-post-meta">
-                    <a href="#">
+                    <a href="/#">
                         Xem chi tiết
                     </a>
                 </p>
